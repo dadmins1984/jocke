4831a6b7b16db9a12331282757912255c3cd6233
dadmins1984 <116805139+dadmins1984@users.noreply.github.com>
2022-11-24 14:14:06 +0100

    Update jocke.ps1

--- jocke.ps1
+++ jocke.ps1
@@ -1,17 +1,17 @@
 $x = New-Object -ComObject Shell.Application
 $x.minimizeall()
-$t =1 
-foreach($i in 1..7){
- start-process "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" https://www.youtube.com/watch?v=zfwJiG2F698#t=2m32s
- }
-do{
 $sh = New-Object -ComObject "Shell.Application"
 $sh.Namespace(17).Items() | 
-  Where-Object { $_.Type -eq "CD Drive" } | 
-  foreach { $_.InvokeVerb("Eject") }
-  Add-Type -AssemblyName System.Windows.Forms
+Where-Object { $_.Type -eq "CD Drive" } | 
+foreach { $_.InvokeVerb("Eject") }
+foreach($i in 1..7){
+start-process "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" https://www.youtube.com/watch?v=zfwJiG2F698#t=2m32s
+}
+Add-Type -AssemblyName System.Windows.Forms
+while ($true)
+{
   $Pos = [System.Windows.Forms.Cursor]::Position
   $x = ($pos.X % 500) + 1
   $y = ($pos.Y % 500) + 1
   [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($x, $y)
- }until($t=0)
+}
